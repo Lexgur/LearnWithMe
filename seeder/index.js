e4f5f42aff34c9e087d83afdc351e65fb9661850
Lexgur <edgaras.malukas@gmail.com>
2025-05-08 13:20:24 +0300

    All tablesa are being dropped

--- seeder/index.js
+++ seeder/index.js
@@ -17,12 +17,27 @@ import { createPool } from 'mysql2/promise';
         console.log('Connected to database.');
 
         let sql;
+        // TABLE DROPS
+        const drops = [
+            'sessions',
+            'payments',
+            'part_contents',
+            'parts',
+            'reviews',
+            'certificates',
+            'user_courses',
+            'courses',
+            'topics',
+            'users'
+        ];
 
-        sql = 'DROP TABLE IF EXISTS users';
-        await con.query(sql);
-        console.log('Dropped users table if it existed.');
+        for (const table of drops) {
+            const sql = `DROP TABLE IF EXISTS \`${table}\``;
+            await con.query(sql);
+            console.log(`Dropped ${table} table if it existed.`);
+        }
 
-        // TODO: Add your faker-based seeding logic here
+        //TODO: Add your faker-based seeding logic here
 
         con.release(); 
         console.log('Database connection released.');
